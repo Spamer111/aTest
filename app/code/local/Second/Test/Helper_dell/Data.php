34f287841c6ba947ad0e6fcf399246b20efd0d40
Spamer111 <spam.by.86@gmail.com>
2017-11-30 15:11:28 +0300

    new structure

--- app/code/local/Second/Test/Helper_dell/Data.php
+++
@@ -1,3 +0,0 @@
-<?php
-class Second_Test_Helper_Data extends Mage_Core_Helper_Abstract {
-}
--- app/code/local/Test/Second/Block/Adminhtml/Second.php
+++ app/code/local/Test/Second/Block/Adminhtml/Second.php
@@ -1,15 +1,12 @@
 <?php
-class Second_Test_Block_Adminhtml_Second extends Mage_Adminhtml_Block_Template
+class Test_Second_Block_Adminhtml_Second extends Mage_Adminhtml_Block_Template
 {
-
     public function getReport(){
         $data = array();
         $categoryCollection = Mage::getModel('catalog/category')->getCollection()->addAttributeToSelect('name');
         foreach($categoryCollection as $key => $category){
-
             $productCollectionPrice = $category->getProductCollection()
                 ->addAttributeToFilter('price',array('gt' => 10));
-
             $productCollectionQty = $category->getProductCollection()
                 ->joinField('qty',
                     'cataloginventory/stock_item',
@@ -18,10 +15,8 @@ class Second_Test_Block_Adminhtml_Second extends Mage_Adminhtml_Block_Template
                     '{{table}}.stock_id=1',
                     'left'
                 )->addAttributeToFilter('qty', array('lt' => 3));
-
             $productCollectionName= $category->getProductCollection()
                 ->addAttributeToFilter('name',array('like' => '%A%'));
-
             $data[$key]['id'] =   $category->getId();
             $data[$key]['name'] =  $category->getName();
             $data[$key]['pricesize'] =  $productCollectionPrice->getSize();
@@ -30,12 +25,4 @@ class Second_Test_Block_Adminhtml_Second extends Mage_Adminhtml_Block_Template
         }
         return $data;
     }
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->setTemplate('second/test.phtml');
-    }
-
-
 }
